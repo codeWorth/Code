aaf731c6322bd238c26c9847e3d7005b00110ef4
Drewpew <agcummings11@gmail.com>
2016-02-03 15:33:54 -0800

    lel

--- Python Code/merjsort.py
+++ Python Code/merjsort.py
@@ -3,11 +3,45 @@ def merjSort(array):
         return array
     else:
         pL = len(array)/2
+        print array[:pL]
+        print array[pL:]
+        print "--------"
         p1 = merjSort(array[:pL])
         p2 = merjSort(array[pL:])
+        print p1
+        print p2
+        print "+++++++"
         n1 = 0
         n2 = 0
+        pL2 = len(p2)
         nL = []
-        while n1 < pL and n2 < pL + 1:
+        while n1 < pL and n2 < pL2:
             if (p1[n1]<p2[n2]):
-                nL.append(
+                nL.append(p1[n1])
+                n1 += 1
+                
+            else:
+                nL.append(p2[n2])
+                n2 += 1
+
+        if n1 == pL:
+            if n2 < pL + 1:
+                nL.extend(p2[n2:])
+
+        else:
+            nL.extend(p1[n1:])
+
+        return nL
+
+while True:
+    array = []
+    num = raw_input("Value: ")
+    while (num != ""):
+        array.append(int(num))
+        num = raw_input("Value: ")
+    
+    print array
+    print "--------"
+    end = merjSort(array)
+    print "Result:"
+    print end
